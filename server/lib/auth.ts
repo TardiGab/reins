21cc1a81f41dd37f8b7af12e63f0324bd7f4fb34
TardiGab <tardigab@gmail.com>
2026-03-02 16:22:14 +0100

    Added better auth config

---
+++ server/lib/auth.ts
@@ -0,0 +1,7 @@
+import { betterAuth } from "better-auth";
+import { dash } from "@better-auth/infra";
+
+export const auth = betterAuth({
+  baseURL: "http://localhost:3000/",
+  plugins: [dash()],
+});
