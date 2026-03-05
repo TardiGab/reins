c39b6c6534d945e11ee2ddb18c020898bdf70b78
TardiGab <tardigab@gmail.com>
2026-03-05 16:01:42 +0100

    better auth login via battlenet working

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+// const { data, error } = await useFetch("/api/version");
+import { authClient } from "~~/server/lib/auth-client";
+const session = authClient.useSession();
+</script>
+
 <template>
+  <!-- <div>
+    <h1>Database Version</h1>
+    <p v-if="data">{{ data.version }}</p>
+    <p v-else-if="error">Error fetching version</p>
+  </div> -->
   <div>
-    <h1>Hello World</h1>
+    <button
+      v-if="!session?.data"
+      @click="() => authClient.signIn.social({ provider: 'battlenet' })"
+    >
+      Connexion avec Battlenet
+    </button>
+    <div>
+      <span v-if="session.data"
+        >Connecté en tant que {{ session.data?.user.name }}
+      </span>
+      <button v-if="session.data" @click="() => authClient.signOut()">
+        Se déconnecter
+      </button>
+    </div>
   </div>
 </template>
--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,7 +1,40 @@
 import { betterAuth } from "better-auth";
+import { genericOAuth } from "better-auth/plugins";
 import { dash } from "@better-auth/infra";
+import pg from "pg";
 
 export const auth = betterAuth({
+  database: new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
   baseURL: "http://localhost:3000/",
-  plugins: [dash()],
+  plugins: [
+    dash(),
+    genericOAuth({
+      config: [
+        {
+          providerId: "battlenet",
+          clientId: process.env.BATTLENET_ID!,
+          clientSecret: process.env.BATTLENET_SECRET!,
+          authorizationUrl: "https://oauth.battle.net/authorize",
+          tokenUrl: "https://oauth.battle.net/token",
+          userInfoUrl: "https://oauth.battle.net/userinfo",
+          getUserInfo: async (tokens) => {
+            const user = await fetch("https://oauth.battle.net/userinfo", {
+              headers: {
+                Authorization: `Bearer ${tokens.accessToken}`,
+              },
+            }).then((res) => res.json());
+
+            return {
+              id: user.id,
+              email: user.battletag, // Use battletag as email workaround
+              emailVerified: false,
+              name: user.battletag,
+            };
+          },
+        },
+      ],
+    }),
+  ],
 });
