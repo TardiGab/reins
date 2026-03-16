e89f02b89cae5a55bef7eb422f985496d5044466
TardiGab <tardigab@gmail.com>
2026-03-16 17:27:05 +0100

    Displaying subcategories names form globalmounts list

--- app/components/MountList.vue
+++ app/components/MountList.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 const { data, error } = await useFetch("/api/mounts");
+
+import mountsGlobal from "@/assets/data/mounts.json";
+
+console.log("Mounts global : ", mountsGlobal);
 </script>
 
 <template>
@@ -15,5 +19,18 @@ const { data, error } = await useFetch("/api/mounts");
     </div>
     <p v-else-if="error">Error fetching mounts : {{ error }}</p>
     <p v-else>Loading...</p>
+  </div>
+  <div>
+    <h2>Montures globales</h2>
+    <!-- <pre v-if="mountsGlobal">{{ mountsGlobal.data }}</pre> -->
+    <div v-if="mountsGlobal">
+      <div v-for="expansion in mountsGlobal" :key="expansion.name">
+        <h3>{{ expansion.name }}</h3>
+        <h4 v-for="subcat in expansion.subcats" :key="subcat.name">
+          {{ subcat.name }}
+        </h4>
+        <!-- <p v-for="mount in expansion.subcats.items">{{  }}</p> -->
+      </div>
+    </div>
   </div>
 </template>
