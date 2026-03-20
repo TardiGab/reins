d592185ad5f26e8b2667a428c88843307b5fbe4e
TardiGab <tardigab@gmail.com>
2026-03-20 12:55:45 +0100

    Changed base urls in auth config to use env variables

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -7,7 +7,7 @@ export const auth = betterAuth({
   database: new pg.Pool({
     connectionString: process.env.DATABASE_URL,
   }),
-  baseURL: "http://localhost:3000/",
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [
     dash(),
     genericOAuth({
