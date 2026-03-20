f53ca502d2047192ee9b5aac473965edfe0eb2ba
TardiGab <tardigab@gmail.com>
2026-03-20 12:51:29 +0100

    Fixed TS error from $WowheadPower

--- app/components/MountList.vue
+++ app/components/MountList.vue
@@ -2,6 +2,16 @@
 import { onMounted } from "vue";
 import mountsGlobal from "@/assets/data/mounts.json";
 const { data, error } = await useFetch("/api/mounts");
+console.log(data);
+
+// Source - https://stackoverflow.com/a/56458070
+// Posted by Titian Cernicova-Dragomir, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-03-20, License - CC BY-SA 4.0
+declare global {
+  interface Window {
+    $WowheadPower: any;
+  }
+}
 
 onMounted(() => {
   if (window.$WowheadPower) {
@@ -11,14 +21,12 @@ onMounted(() => {
 </script>
 
 <template>
-  <!-- <div>
+  <div>
     <h2>Montures possédées</h2>
     <div v-if="data">
       <ul>
         <li v-for="mounts in data">
-          <a
-            :href="`https://wowhead.com/mount/${mounts.mount.id}?domain=fr`"
-          >
+          <a :href="`https://wowhead.com/mount/${mounts.mount.id}`">
             {{ mounts.mount.name }}
           </a>
         </li>
@@ -26,10 +34,10 @@ onMounted(() => {
     </div>
     <p v-else-if="error">Error fetching mounts : {{ error }}</p>
     <p v-else>Loading...</p>
-  </div> -->
+  </div>
   <div>
     <h2>Montures globales</h2>
-    <div v-if="mountsGlobal && data">
+    <div>
       <div v-for="expansion in mountsGlobal" :key="expansion.name">
         <h3>{{ expansion.name }}</h3>
         <div v-for="subcat in expansion.subcats" :key="subcat.name">
@@ -44,16 +52,8 @@ onMounted(() => {
               >
                 <img
                   :src="`https://wow.zamimg.com/images/wow/icons/medium/${mount.icon?.toLowerCase()}.jpg`"
-                  alt=""
-                  srcset=""
                 />
-                <!-- <img
-                  :src="`https://render.worldofwarcraft.com/eu/icons/56/ability_mount_drake_blue.jpg`"
-                  alt=""
-                  srcset=""
-                /> -->
                 <span>{{ mount.name }}</span>
-                <!-- {{ mount.name }} -->
               </a>
             </li>
           </ul>
