f2e139e92a21a19bbd2aa4f0ac4d84252278e96d
TardiGab <tardigab@gmail.com>
2026-03-30 17:32:22 +0200

    Force reload webpage after user logout

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
 import { authClient } from "~~/server/lib/auth-client";
 const session = authClient.useSession();
+
+function logout() {
+  authClient.signOut();
+  setTimeout(() => {
+    window.location.reload();
+  }, 100);
+}
 </script>
 
 <template>
   <header>
     <h1>Reins</h1>
-    <button
-      v-if="!session?.data"
-      @click="() => authClient.signIn.social({ provider: 'battlenet' })"
-    >
-      Connexion avec Battlenet
-    </button>
-    <div>
-      <p v-if="session.data">Bonjour, {{ session.data?.user.name }}</p>
-      <button v-if="session.data" @click="() => authClient.signOut()">
-        Se déconnecter
+    <div v-if="!session?.data">
+      <button
+        @click="() => authClient.signIn.social({ provider: 'battlenet' })"
+      >
+        Connexion avec Battlenet
       </button>
+    </div>
+    <div v-if="session.data">
+      <p>Bonjour, {{ session.data?.user.name }}</p>
+      <button @click="logout">Se déconnecter</button>
     </div>
   </header>
 </template>
