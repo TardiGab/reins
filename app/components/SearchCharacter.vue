492234631d671fa7b098c60afcf9307f9f20aedb
TardiGab <tardigab@gmail.com>
2026-04-16 00:23:29 +0200

    Combobox with button instead

--- app/components/SearchCharacter.vue
+++ app/components/SearchCharacter.vue
@@ -35,12 +35,25 @@ let realmsUsId: number[] = [];
 // }
 
 // Combobox créé à l'aide de ce tutoriel : https://www.youtube.com/watch?v=KlMIf0_48b8, à mettre en page
+const regions = ref(["EU", "US", "KR", "TW"]);
 
 const selectedRegion = ref("");
 const searchTerm = ref("");
 const showList = ref(false);
 const boxContainer = ref(null);
 const searchInput = ref(null);
+
+function selectRegion(region: string) {
+  selectedRegion.value = region;
+  searchTerm.value = "";
+  showList.value = false;
+}
+
+const filteredRegions = computed(() => {
+  return regions.value.filter((region) =>
+    region.toLocaleLowerCase().startsWith(searchTerm.value.toLocaleLowerCase()),
+  );
+});
 
 function closeDropdown(event: any) {
   if (
@@ -59,41 +72,32 @@ onMounted(() => {
 onUnmounted(() => {
   window.addEventListener("click", closeDropdown);
 });
-
-const regions = ref(["EU", "US", "KR", "TW"]);
-
-function selectRegion(region: string) {
-  selectedRegion.value = region;
-  searchTerm.value = "";
-  showList.value = false;
-}
-
-const filteredRegions = computed(() => {
-  return regions.value.filter((region) =>
-    region.toLocaleLowerCase().startsWith(searchTerm.value.toLocaleLowerCase()),
-  );
-});
 </script>
 
 <template>
-  <div class="combobox-container">
-    <div class="box-list">
-      <input
-        type="text"
-        v-model="searchTerm"
-        ref="searchInput"
-        :placeholder="selectedRegion || 'Region'"
-        @click="showList = !showList"
-        class=""
-      />
-      <span>{{ searchTerm || selectedRegion }}</span>
-      <div class="region-list" v-show="showList">
-        <span
-          v-for="region in filteredRegions"
-          :key="region"
-          @click="selectRegion(region)"
-          >{{ region }}</span
-        >
+  <div class="region-choice">
+    <div class="region-choice__container" ref="boxContainer">
+      <button @click="showList = !showList" class="region-choice__button">
+        {{ selectedRegion || "Select a region" }}
+      </button>
+      <div class="input-container" v-if="showList">
+        <input
+          type="text"
+          v-model="searchTerm"
+          ref="searchInput"
+          class="region-choice__input"
+          placeholder="Select a region"
+        />
+        <div class="region-list">
+          <span
+            v-for="region in filteredRegions"
+            :key="region"
+            @click="selectRegion(region)"
+            class="region-list__value"
+          >
+            {{ region }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -143,21 +147,63 @@ const filteredRegions = computed(() => {
   </form> -->
 </template>
 
-<style scoped>
-.combobox-container {
+<style scoped lang="scss">
+.input-container {
   position: relative;
+  &::after {
+    content: attr(data-value) "";
+    font: inherit;
+    min-width: 2px;
+    height: auto;
+    position: absolute;
+    left: 1rem;
+    top: 0.5rem;
+    z-index: 10;
+  }
 }
-.box-container {
-}
-.box-list {
-  position: absolute;
-  z-index: 10;
-  width: 100%;
-  overflow: hidden;
+
+.region-choice {
+  position: relative;
+  &__container {
+    position: absolute;
+    z-index: 10;
+    min-width: 10%;
+    overflow: hidden;
+  }
+  &__button {
+    width: 100%;
+  }
+  &__input {
+    background-color: #191612;
+    border: 1px solid $border-container;
+    color: $dark-gray;
+    font-size: $small;
+    border-radius: 0.5rem;
+    padding: 0.5rem 1rem;
+    line-height: 1;
+  }
+  &__value {
+    position: absolute;
+    top: 0.7rem;
+    left: 1rem;
+    color: $dark-gray;
+    font-size: $small;
+    line-height: 1;
+  }
 }
 .region-list {
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
+  padding: 1rem;
+
+  &__value {
+    padding: 0.25rem;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    &:hover {
+      background-color: $dark-gray;
+    }
+  }
 }
 </style>
