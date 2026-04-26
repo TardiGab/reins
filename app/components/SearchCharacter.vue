ce8724a0da12d04e79a0e3bfd773b18c754d73f4
TardiGab <tardigab@gmail.com>
2026-04-26 17:39:50 +0200

    querying two different profiles working

--- app/components/SearchCharacter.vue
+++ app/components/SearchCharacter.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+interface CharactersMounts {
+  mounts: {
+    mount: {
+      key: { href: string };
+      name: string;
+      id: number;
+    };
+  };
+}
+
 const { data: accessToken } = await useFetch("/api/access-token");
 
 const regionChoosed = ref<string>("");
@@ -11,43 +21,122 @@ const realmSelected = (realm: string) => {
   realmChoosed.value = realm;
 };
 
-const character = ref<string>();
+const characterSearch = ref<string>();
+
+const baseCharacterName = ref<string>();
+const comparedCharacterName = ref<string>();
+
+const baseCharacterSearch = ref<string>();
+const comparedCharacterSearch = ref<string>();
 
 const {
   data: characterMounts,
-  execute,
-  status,
+  execute: go,
+  status: loading,
 } = await useLazyFetch("/api/character-mounts", {
   query: {
     region: regionChoosed,
     realm: realmChoosed,
-    character: character,
+    character: characterSearch,
     accessToken: accessToken.value.access_token,
   },
   watch: false,
   immediate: false,
 });
 
-const search = async () => {
-  await execute();
-  console.log(characterMounts.value);
+const baseCharacterMountsData = ref<CharactersMounts>();
+const baseSearch = async () => {
+  characterSearch.value = baseCharacterSearch.value;
+  await go();
+  // Histoire d'éviter que lorsqu'on relance la recherche, la valeur du span change en temps réel
+  baseCharacterName.value = baseCharacterSearch.value;
+  baseCharacterMountsData.value = characterMounts.value;
+  console.log(
+    `${baseCharacterName.value} mounts data:`,
+    baseCharacterMountsData.value,
+  );
+};
+
+const comparedCharacterMountsData = ref<CharactersMounts>();
+const comparedSearch = async () => {
+  characterSearch.value = comparedCharacterSearch.value;
+  await go();
+  // Histoire d'éviter que lorsqu'on relance la recherche, la valeur du span change en temps réel
+  comparedCharacterName.value = comparedCharacterSearch.value;
+  comparedCharacterMountsData.value = characterMounts.value;
+  console.log(
+    `${comparedCharacterName.value} mounts data:`,
+    comparedCharacterMountsData.value,
+  );
 };
 </script>
 
 <template>
-  <div class="search">
-    <SelectRegion @region="regionSelected" />
-    <SelectRealm :region-choosed="regionChoosed" @realm="realmSelected" />
-    <input
-      type="text"
-      v-model="character"
-      placeholder="Character's name"
-      @change="search"
-    />
-    <button @click="search">Search</button>
+  <div class="compare">
+    <div class="left">
+      <div class="search" v-if="!baseCharacterMountsData">
+        <SelectRegion @region="regionSelected" />
+        <SelectRealm :region-choosed="regionChoosed" @realm="realmSelected" />
+        <input
+          type="text"
+          v-model="baseCharacterSearch"
+          placeholder="Character's name"
+          @change="baseSearch"
+        />
+        <button @click="baseSearch">Search</button>
+      </div>
+      <pre v-if="loading === 'pending'">Loading...</pre>
+      <div v-if="loading === 'success'">
+        <span>{{ baseCharacterName }}'s mount collection</span>
+        <!-- <pre>{{ baseCharacterMounts }}</pre> -->
+        <ul>
+          <li
+            v-for="mounts in baseCharacterMountsData"
+            :key="mounts.mount.key.href"
+          >
+            {{ mounts.mount.name }}
+          </li>
+        </ul>
+      </div>
+      <pre v-if="loading === 'error'">
+        The character named {{ baseCharacterName }} was not found.
+      </pre>
+    </div>
+    <div class="right">
+      <div class="search" v-if="!comparedCharacterMountsData">
+        <SelectRegion @region="regionSelected" />
+        <SelectRealm :region-choosed="regionChoosed" @realm="realmSelected" />
+        <input
+          type="text"
+          v-model="comparedCharacterSearch"
+          placeholder="Character's name"
+          @change="comparedSearch"
+        />
+        <button @click="comparedSearch">Search</button>
+      </div>
+      <pre v-if="loading === 'pending'">Loading...</pre>
+      <div v-if="loading === 'success'">
+        <span>{{ comparedCharacterName }}'s mount collection</span>
+        <ul>
+          <li
+            v-for="mounts in comparedCharacterMountsData"
+            :key="mounts.mount.key.href"
+          >
+            {{ mounts.mount.name }}
+          </li>
+        </ul>
+      </div>
+      <pre v-if="loading === 'error'">
+        The character named {{ comparedCharacterName }} was not found.
+      </pre>
+    </div>
   </div>
-  <pre v-if="status === 'pending'">Loading...</pre>
-  <pre v-else>{{ characterMounts }}</pre>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.compare {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+</style>
