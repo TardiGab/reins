da2a65edd12fe86ec168485887f907488e498257
TardiGab <tardigab@gmail.com>
2026-05-05 15:24:18 +0200

    Changed random mount button icon

--- app/components/RandomMount.vue
+++ app/components/RandomMount.vue
@@ -49,13 +49,15 @@ function getRandomMount() {
       <button class="random-mount__btn" @click="getRandomMount">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="22"
-          height="22"
-          viewBox="0 0 512 512"
+          width="1.5em"
+          height="1.5em"
+          viewBox="0 0 20 20"
         >
           <path
             fill="#FFD100"
-            d="M341.3 28.3v85.3H128c-70.7 0-128 57.3-128 128c0 21.5 5.8 41.4 15.2 59.2L68 263.2c-2.4-6.8-4-13.9-4-21.5c0-35.4 28.7-64 64-64h213.3V263L512 156.3V135zM444 262.8c2.4 6.8 4 13.9 4 21.5c0 35.4-28.6 64-64 64H170.7V263L0 369.7V391l170.7 106.7v-85.3H384c70.7 0 128-57.3 128-128c0-21.5-5.8-41.4-15.2-59.2z"
+            d="M15.854 4.146a.5.5 0 0 0-.708.708l1.15 1.148c-3.323.073-5.283 1.902-7.105 3.603l-.032.03C7.277 11.39 5.533 13 2.5 13a.5.5 0 0 0 0 1c3.447 0 5.45-1.87 7.31-3.605l.031-.03c1.838-1.715 3.545-3.29 6.45-3.362l-1.145 1.144a.5.5 0 0 0 .708.707l2-2a.5.5 0 0 0 0-.708zM2.5 6c2.81 0 4.661 1.243 6.256 2.642l-.28.261q-.23.218-.457.425C6.517 8.023 4.92 7 2.5 7a.5.5 0 0 1 0-1m13.795 7.998c-2.696-.059-4.495-1.275-6.051-2.64l.28-.261q.23-.218.457-.425c1.458 1.267 3.008 2.268 5.31 2.325l-1.145-1.143a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.707z"
+            stroke-width="0.5"
+            stroke="#FFD100"
           />
         </svg>
       </button>
@@ -78,6 +80,7 @@ function getRandomMount() {
 
 <style scoped lang="scss">
 .random-mount {
+  font-size: $main-size;
   &__heading {
     display: flex;
     align-items: center;
@@ -90,6 +93,9 @@ function getRandomMount() {
     background-color: transparent;
     cursor: pointer;
     transition: all 0.3s ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     &:hover {
       filter: brightness(80%);
     }
