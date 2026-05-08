72de398160f38e8201bccbba59a724b573646b61
TardiGab <tardigab@gmail.com>
2026-05-08 14:33:06 +0200

    Added disabled state to compare component search input

--- app/components/CompareSearchCharacter.vue
+++ app/components/CompareSearchCharacter.vue
@@ -95,6 +95,7 @@ const search = async () => {
         placeholder="Character's name"
         @change="search"
         class="search__input"
+        :disabled="!realmChoosed"
       />
     </div>
 
@@ -163,6 +164,12 @@ const search = async () => {
         inset 0px 0px 0px 2px $dark-gray,
         inset 0 0 0 3px black;
       outline: none;
+    }
+    &:disabled {
+      cursor: not-allowed;
+      &:hover {
+        background-color: $button-bg-dark;
+      }
     }
   }
   &__button {
