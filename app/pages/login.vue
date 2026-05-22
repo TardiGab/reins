06e49f8c8b0a4140222fd628d9707bbfd1437862
TardiGab <tardigab@gmail.com>
2026-05-22 19:42:54 +0200

    Added stables background image to login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -11,35 +11,62 @@ watch(
 
 <template>
   <div class="login">
-    <h2 class="login__h2">Access your stable</h2>
-    <div class="login__oauth">
-      <p class="login__desc">
-        Link your Battle.net account to Reins and see what are the mounts you’re
-        missing!
-      </p>
-      <LoginButton />
-      <p class="login__disclamer">
-        We do not have access to your email adress or password. Your
-        informations will directly go to Blizzard.
-      </p>
-    </div>
-    <div class="login__divider"><span>Or</span></div>
-    <div class="login__guest">
-      <h3 class="login__h3">Look up your character as a guest.*</h3>
-      <p class="login__disclamer">
-        *Some features may be limited to logged-in users.
-      </p>
-      <LoginSearchCharacter class="login__search" />
+    <div class="login__container">
+      <h2 class="login__h2">Access your stable</h2>
+      <div class="login__oauth">
+        <p class="login__desc">
+          Link your Battle.net account to Reins and see what are the mounts
+          you’re missing!
+        </p>
+        <LoginButton />
+        <p class="login__disclamer">
+          We do not have access to your email adress or password. Your
+          informations will directly go to Blizzard.
+        </p>
+      </div>
+      <div class="login__divider"><span>Or</span></div>
+      <div class="login__guest">
+        <h3 class="login__h3">Look up your character as a guest.*</h3>
+        <p class="login__disclamer">
+          *Some features may be limited to logged-in users.
+        </p>
+        <LoginSearchCharacter class="login__search" />
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .login {
-  max-width: 500px;
+  position: relative;
+  max-width: 1440px;
   margin: auto;
+  border: 2px solid $border-container;
+  @include border-radius(1rem, true);
+  padding: 2rem;
+  overflow: hidden;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    background-image: url("/images/stable.webp");
+    background-repeat: no-repeat;
+    background-size: cover;
+    opacity: 0.2;
+    width: 100%;
+    height: 100%;
+  }
+  &__container {
+    max-width: 500px;
+    position: relative;
+    margin: auto;
+    z-index: 1;
+  }
   &__h2 {
     @include gradient-heading($h2-size);
+    margin-top: 0;
   }
   &__h3 {
     font-size: $h3-size;
