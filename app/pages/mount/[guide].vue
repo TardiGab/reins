6757d654210d899bb89057ae4f64859d9e5416a5
TardiGab <tardigab@gmail.com>
2026-05-25 20:45:41 +0200

    started guide page template

---
+++ app/pages/mount/[guide].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useRoute } from "#app";
+import mounts from "@/assets/data/mounts.json";
+
+const route = useRoute();
+
+let mountInfos;
+
+mounts.forEach((category) => {
+  category.subcats.forEach((subcat) => {
+    mountInfos = computed(() => {
+      // console.log(subcat.items.find((mount) => mount.ID == route.params.guide as number));
+      // return subcat.items.find((mount) => mount.ID === route.params.guide);
+    });
+  });
+});
+
+useHead({
+  title: `Reins | ${route.params.guide}`,
+});
+</script>
+
+<template>
+  <p>{{ mountInfos }}</p>
+</template>
