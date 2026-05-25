eb577f378b3deac6893cf9b6cd6cfd3754467504
TardiGab <tardigab@gmail.com>
2026-05-25 13:03:44 +0200

    Added pin button to random mount

--- app/components/RandomMount.vue
+++ app/components/RandomMount.vue
@@ -45,6 +45,25 @@ function getRandomMount() {
   randomResponse.value = getRandomInInclusive(0, randomMountArrayLength);
   window.$WowheadPower.refreshLinks();
 }
+
+async function pinMount(
+  mountName: string,
+  mountId: number,
+  mountIcon: string,
+  userId: string | undefined,
+) {
+  await $fetch("/api/pin-mount", {
+    method: "POST",
+    body: {
+      mountName: mountName,
+      mountId: mountId,
+      mountIcon: mountIcon,
+      userId: userId,
+    },
+  });
+  await refreshNuxtData("pinned-mounts");
+  window.$WowheadPower.refreshLinks();
+}
 </script>
 
 <template>
@@ -83,6 +102,29 @@ function getRandomMount() {
         />
         <span>{{ randomMountArray[randomResponse].name }}</span>
       </a>
+      <button
+        @click="
+          pinMount(
+            randomMountArray[randomResponse].name,
+            randomMountArray[randomResponse].ID,
+            randomMountArray[randomResponse].icon,
+            session.data?.user.id,
+          )
+        "
+        class="mount-item__pin-btn"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="#FFD100"
+            d="M4.146.146A.5.5 0 0 1 4.5 0h7a.5.5 0 0 1 .5.5c0 .68-.342 1.174-.646 1.479c-.126.125-.25.224-.354.298v4.431l.078.048c.203.127.476.314.751.555C12.36 7.775 13 8.527 13 9.5a.5.5 0 0 1-.5.5h-4v4.5c0 .276-.224 1.5-.5 1.5s-.5-1.224-.5-1.5V10h-4a.5.5 0 0 1-.5-.5c0-.973.64-1.725 1.17-2.189A6 6 0 0 1 5 6.708V2.277a3 3 0 0 1-.354-.298C4.342 1.674 4 1.179 4 .5a.5.5 0 0 1 .146-.354"
+          />
+        </svg>
+      </button>
     </div>
     <div v-else class="random-mount--message">
       <span>Please log in to get a random mount suggestion</span>
