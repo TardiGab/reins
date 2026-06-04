1c24bffa9daca149372637bbfb2d2edea173be5a
TardiGab <tardigab@gmail.com>
2026-06-04 11:19:18 +0200

    added general classes to content wh links

--- app/components/guides/Content.vue
+++ app/components/guides/Content.vue
@@ -69,10 +69,7 @@ const { data: page } = await useAsyncData(route.path, () => {
   <div class="guide">
     <div class="guide-heading">
       <h2 class="guide-heading__h2">
-        <a
-          :href="`https://wowhead.com/ptr-2/mount/${mountInfos.ID}`"
-          target="_blank"
-        >
+        <a href="#" :data-wowhead="`item=${mountInfos.itemId}`" rel="np">
           <img
             :src="`https://wow.zamimg.com/images/wow/icons/large/${mountInfos.icon?.toLowerCase()}.jpg`"
             class="mount-item__icon"
@@ -95,8 +92,15 @@ const { data: page } = await useAsyncData(route.path, () => {
           <h2 class="guide__h2">Guide Not Found</h2>
           <p>
             Oops! The content you're looking for doesn't exist (yet). Feel free
-            to <a href="" class="guide__link">contribute</a> and create a guide
-            for this mount!
+            to
+            <a
+              href="https://github.com/TardiGab/reins/tree/main/content"
+              class="guide__link"
+              target="_blank"
+            >
+              contribute
+            </a>
+            and create a guide for this mount!
           </p>
         </div>
         <div class="mount-faction">
@@ -130,18 +134,20 @@ const { data: page } = await useAsyncData(route.path, () => {
           <div class="display-wrapper__links">
             <a
               :href="`https://wowhead.com/ptr-2/mount/${mountInfos.ID}`"
-              class="display-wrapper__wh-link"
+              class="button-gray"
               target="_blank"
+              rel="np"
             >
-              <span>More on Wowhead</span>
+              <span class="button-label">More on Wowhead</span>
             </a>
 
             <a
               :href="`https://wowhead.com/ptr-2/mount/${mountInfos.ID}#modelviewer`"
-              class="display-wrapper__wh-link"
+              class="button-gray"
               target="_blank"
+              rel="np"
             >
-              <span>View 3D model on Wowhead</span>
+              <span class="button-label">View 3D model on Wowhead</span>
             </a>
           </div>
         </div>
--- app/components/guides/ContentNotFound.vue
+++ app/components/guides/ContentNotFound.vue
@@ -82,6 +82,7 @@ const { data: creatureDisplay } = await useFetch(
         <a
           :href="`https://wowhead.com/ptr-2/mount/${mountInfos.ID}`"
           target="_blank"
+          rel="np"
         >
           <img
             :src="`https://wow.zamimg.com/images/wow/icons/large/${mountInfos.icon?.toLowerCase()}.jpg`"
@@ -100,8 +101,15 @@ const { data: creatureDisplay } = await useFetch(
           <h2 class="guide__h2">Guide Not Found</h2>
           <p class="guide__p">
             Oops! The content you're looking for doesn't exist (yet). Feel free
-            to <a href="" class="guide__link">contribute</a> and create a guide
-            for this mount!
+            to
+            <a
+              href="https://github.com/TardiGab/reins/tree/main/content"
+              class="guide__link"
+              target="_blank"
+            >
+              contribute
+            </a>
+            and create a guide for this mount!
           </p>
         </div>
         <div class="mount-faction">
@@ -135,17 +143,19 @@ const { data: creatureDisplay } = await useFetch(
           <div class="display-wrapper__links">
             <a
               :href="`https://wowhead.com/ptr-2/mount/${mountInfos.ID}`"
-              class="display-wrapper__wh-link"
+              class="button-gray"
               target="_blank"
+              rel="np"
             >
-              <span>More on Wowhead</span>
+              <span class="button-label">More on Wowhead</span>
             </a>
             <a
               :href="`https://wowhead.com/ptr-2/mount/${mountInfos.ID}#modelviewer`"
-              class="display-wrapper__wh-link"
+              class="button-gray"
               target="_blank"
+              rel="np"
             >
-              <span>View 3D model on Wowhead</span>
+              <span class="button-label">View 3D model on Wowhead</span>
             </a>
           </div>
         </div>
