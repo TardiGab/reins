e3590b075e8bb606d3d2087974b805f187a25831
TardiGab <tardigab@gmail.com>
2026-07-12 15:02:54 +0200

    chore: deleted unused css on credits page

--- app/pages/credits.vue
+++ app/pages/credits.vue
@@ -17,43 +17,5 @@ const { data: page } = await useAsyncData(route.path, () => {
 .credits {
   max-width: 800px;
   margin: auto;
-  h2 {
-    @include gradient-heading($h2-size, 800);
-    text-align: left;
-    margin-bottom: 1rem;
-  }
-  h3 {
-    font-size: $h3-size;
-    text-align: left;
-    margin-bottom: 0.5rem;
-    a {
-      color: white;
-    }
-  }
-  h4 {
-    font-size: $main-size;
-    text-align: left;
-    margin-bottom: 0.5rem;
-    a {
-      color: white;
-    }
-  }
-  p,
-  li {
-    font-size: $main-size;
-    line-height: 1.4;
-    a {
-      color: white;
-      text-decoration: none;
-      transition: filter 0.3s ease;
-      &:hover {
-        filter: brightness(0.7);
-      }
-      &:visited {
-        color: white;
-        filter: brightness(0.7);
-      }
-    }
-  }
 }
 </style>
